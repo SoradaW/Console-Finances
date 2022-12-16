9f96b73c74282f601d0eeb2b421c134a09dfa2d6
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2022-12-16 20:42:53 +0000

    Added JS code

--- starter/index.js
+++ starter/index.js
@@ -1,10 +1,8 @@
 const finances = [
-    ['Jan-2010', 300],
+    ['Jan-2010', 200],
     ['Feb-2010', 300],
-    ['Mar-2010', 500],
-    ['Apr-2010', -500],
-    ['May-2010', -200],
-    ['Jun-2010', 200]
+    ['Mar-2010', -200],
+    ['Apr-2010', 500],
 ];
 
 // The total number of months included in the dataset.
@@ -24,14 +22,31 @@ console.log('Total amount of Profit/Losses over the entire period: ', totalProfi
 // You will need to track what the total change in profits is from month to month and then find the average.
 
 const trackChange = finances.map((el) => el[1]);
-const avgProfitLoss = trackChange.reduce((accValue, curValue) => accValue + curValue) / totalMonths; // (Total/Number of months)
+const avgProfitLoss = trackChange.reduce((accValue, curValue) => accValue + curValue, 0) / (totalMonths); // (Total/Number of months)
 console.log('The average of the changes in Profit/Losses: ', avgProfitLoss);
 
 // The greatest increase in profits (date and amount) over the entire period.
-
+// The greatest decrease in profits (date and amount) over the entire period.
 const bestMonth = {
     monthName: '',
     profit: 0,
 };
 
-// The greatest decrease in losses (date and amount) over the entire period.
+const badMonth = {
+    monthName: '',
+    profit: 0,
+};
+
+finances.forEach((month) => {
+    if (month[1] > bestMonth.profit) {
+        bestMonth.monthName = month[0];
+        bestMonth.profit = month[1];
+    }
+    if (month[1] < badMonth.profit) {
+        badMonth.monthName = month[0];
+        badMonth.profit = month[1];
+    }
+    return {bestMonth, badMonth}
+});
+console.log('The greatest increase in profit is: ', bestMonth);
+console.log('The greatest decrease in profit is: ', badMonth);
