f259893c0a52883f08210ad2d279b0bb81dc20d2
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2022-12-16 19:14:32 +0000

    Completed 2 tasks on tested file

--- starter/index.js
+++ starter/index.js
@@ -1,18 +1,30 @@
-const finances1 = [
+const finances = [
     ['Jan-2010', 300],
-    ['Feb-2010', 100],
+    ['Feb-2010', 300],
     ['Mar-2010', 500],
-    ['Apr-2010', -300],
-    ['May-2010', 200],
-    ['Jun-2010', 200]
+    ['Apr-2010', -500],
+    ['May-2010', -200],
+    ['Jun-2010', 100]
 ];
 
-let totalMonths1 = finances1.length;
-console.log('Total number of months: ', totalMonths1);
+// The total number of months included in the dataset.
 
-const profitLoss1 = finances1.filter((el) => el[1] > 0);
-const totalProfitLoss1 = profitLoss1
+let totalMonths = finances.length;
+console.log('Total number of months: ', totalMonths);
+
+// The net total amount of Profit/Losses over the entire period.
+
+const profitLoss = finances.filter((el) => el[1]);
+const totalProfitLoss = profitLoss
     .map((el) => el [1])
-    .reduce((accVal, curVal) => accVal + curVal, 0);
+    .reduce((accValue, curValue) => accValue + curValue);
 
-console.log('Total amount of Profit/Losses over the entire period: ', totalProfitLoss1);
+console.log('Total amount of Profit/Losses over the entire period: ', totalProfitLoss);
+
+// The average of the changes in Profit/Losses over the entire period.
+
+// You will need to track what the total change in profits is from month to month and then find the average. (Total/Number of months)
+
+// The greatest increase in profits (date and amount) over the entire period.
+
+// The greatest decrease in losses (date and amount) over the entire period.
